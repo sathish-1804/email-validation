565ca6e23eec5fa958cadf39371059244bc31f61
Sathish <107974739+sathish-1804@users.noreply.github.com>
2023-08-26 15:52:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from streamlit_extras.metric_cards import style_metric_cards
 st.set_page_config(
     page_title="Email verification",
     page_icon="✅",
-    layout="wide",
+    layout="centered",
 )
 
 def label_email(email):
